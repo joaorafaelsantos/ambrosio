d906d7ae14cbedfd47f3d2593b72b7ff2a30f622
João Santos <joaorafaelsantos@outlook.pt>
2021-07-29 10:39:03 +0100

    Added topic subscription

--- app/src/App.js
+++ app/src/App.js
@@ -1,7 +1,13 @@
 import React, {useEffect} from 'react';
 import {View, Text} from 'react-native';
 
+import messaging from '@react-native-firebase/messaging';
+
 function App() {
+  useEffect(() => {
+    messaging().subscribeToTopic('users');
+  }, []);
+
   return (
     <View>
       <Text>Ambrosio</Text>
